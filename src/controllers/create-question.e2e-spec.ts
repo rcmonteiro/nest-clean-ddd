351d4fdf230e807e4613efae082f3be4db970a97
Ricardo Monteiro <rcmonteiro@gmail.com>
2024-04-25 17:12:13 -0300

    test: create and fetch questions e2e tests

---
+++ src/controllers/create-question.e2e-spec.ts
@@ -0,0 +1,52 @@
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+
+describe('Create Question (e2e)', () => {
+  let app: INestApplication
+  let db: PrismaService
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    db = moduleRef.get(PrismaService)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[POST] /questions', async () => {
+    const user = await db.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'john.doe@me.com',
+        password: '123123',
+      },
+    })
+
+    const accessToken = jwt.sign({
+      sub: user.id,
+    })
+
+    const response = await request(app.getHttpServer())
+      .post('/questions')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        title: 'New question title',
+        content: 'New question content',
+      })
+
+    const question = await db.question.findFirst()
+    expect(response.status).toBe(201)
+    expect(question).toEqual(
+      expect.objectContaining({ title: 'New question title' }),
+    )
+  })
+})
---
+++ src/controllers/fetch-recent.e2e-spec.ts
@@ -0,0 +1,66 @@
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+
+describe('Fetch Recent Question (e2e)', () => {
+  let app: INestApplication
+  let db: PrismaService
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    db = moduleRef.get(PrismaService)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[GET] /questions', async () => {
+    const user = await db.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'john.doe@me.com',
+        password: '123123',
+      },
+    })
+
+    const accessToken = jwt.sign({
+      sub: user.id,
+    })
+
+    await db.question.createMany({
+      data: [
+        {
+          title: 'New question title 1',
+          slug: 'new-question-title-1',
+          content: 'New question content-1',
+          authorId: user.id,
+        },
+        {
+          title: 'New question title 2',
+          slug: 'new-question-title-2',
+          content: 'New question content-2',
+          authorId: user.id,
+        },
+      ],
+    })
+
+    const response = await request(app.getHttpServer())
+      .get('/questions')
+      .set('Authorization', `Bearer ${accessToken}`)
+
+    expect(response.status).toBe(200)
+    expect(response.body.questions).toHaveLength(2)
+    expect(response.body.questions).toEqual([
+      expect.objectContaining({ title: 'New question title 1' }),
+      expect.objectContaining({ title: 'New question title 2' }),
+    ])
+  })
+})
