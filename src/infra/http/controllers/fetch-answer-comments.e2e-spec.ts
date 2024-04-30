abef29b8695c09c94c8b1e62143e3eae6f6ca361
Ricardo Monteiro <rcmonteiro@gmail.com>
2024-04-30 06:47:03 -0300

    test: acertando o e2e test do controller de responder pergunta

--- src/infra/http/controllers/fetch-answer-comments.e2e-spec.ts
+++ src/infra/http/controllers/fetch-answer-comments.e2e-spec.ts
@@ -40,7 +40,7 @@ describe('Fetch answer comments (E2E)', () => {
   })
 
   test('[GET] /answers/:answerId/comments', async () => {
-    const user = await studentFactory.makeDbStudent()
+    const user = await studentFactory.makeDbStudent({ name: 'John Doe' })
 
     const accessToken = jwt.sign({ sub: user.id.toString() })
 
@@ -76,8 +76,14 @@ describe('Fetch answer comments (E2E)', () => {
     expect(response.statusCode).toBe(200)
     expect(response.body).toEqual({
       comments: expect.arrayContaining([
-        expect.objectContaining({ content: 'Comment 01' }),
-        expect.objectContaining({ content: 'Comment 01' }),
+        expect.objectContaining({
+          content: 'Comment 01',
+          authorName: 'John Doe',
+        }),
+        expect.objectContaining({
+          content: 'Comment 02',
+          authorName: 'John Doe',
+        }),
       ]),
     })
   })
--- src/infra/http/controllers/fetch-answer-comments.ts
+++ src/infra/http/controllers/fetch-answer-comments.ts
@@ -8,7 +8,7 @@ import {
   Query,
 } from '@nestjs/common'
 import { z } from 'zod'
-import { CommentPresenter } from '../presenters/comment-presenter'
+import { CommentWithAuthorPresenter } from '../presenters/comment-with-author-presenter'
 
 const pageQueryParamSchema = z
   .string()
@@ -39,8 +39,8 @@ export class FetchAnswerComments {
       throw new BadRequestException()
     }
 
-    const answerComments = result.value.answerComments
+    const comments = result.value.comments
 
-    return { comments: answerComments.map(CommentPresenter.toHTTP) }
+    return { comments: comments.map(CommentWithAuthorPresenter.toHTTP) }
   }
 }
